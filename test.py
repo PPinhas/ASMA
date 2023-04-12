de549ce8d30b8ff060a797e71359249135d38a21
Pedro Pinheiro <up201906788@fe.up.pt>
2023-04-13 00:53:48 +0100

    slight improvement

--- test.py
+++ test.py
@@ -10,19 +10,18 @@ num_repetitions = 3
 
 for i in range((len(df.index)-1)//(num_repetitions+1)):
     start = i*(num_repetitions+1)
-    print("start is ", start)
     for j in range(num_players):
         agent = df.iloc[start+1, j]
-        print("agent is ", agent)
-
-        print("df.iloc[start+2] is ", df.iloc[start+2])
         player_data = df.iloc[start+2:start+2+num_repetitions, j].astype(int)
-        print("player_data is ", player_data)
-        plt.plot(player_data, label=f'{players[j]} ({agent})')
+
+        #plt.plot(player_data, label=f'{players[j]} ({agent})')
+        plt.plot(range(len(player_data)), player_data, label=f'{players[j]} ({agent})')
+
 
 
     plt.title('Player Scores Over Time')
     plt.xlabel('Game Number')
     plt.ylabel('Score')
+    plt.xticks(range(len(player_data)), ['Game {}'.format(x+1) for x in range(len(player_data))])
     plt.legend()
     plt.show()
